002ef54f90597c9f4e3db7d707282c41af0860af
jleflang <jleflang@ucmerced.edu>
2020-05-30 22:40:58 -0700

    ReadVal work in progress still

--- project_leflangj.asm
+++ project_leflangj.asm
@@ -12,17 +12,24 @@ INCLUDE Irvine32.inc
 ARRAYSIZE EQU 10
 
 ;--------------------------------------
-getString MACRO bufferaddr:REQ, buffertyp:REQ
+getString MACRO bufferaddr, buffertyp, bufsize, prompt
 ; Gets an input string to return the input in buffer.
-; Avoid passing arguments in edx, ecx.
+; Avoid passing arguments in edx, ecx, eax.
 ;--------------------------------------
 	push	edx
 	push	ecx
+	push	eax
+
+	mov		edx, prompt
+	call	WriteString
 
 	mov		edx, bufferaddr
 	mov		ecx, buffertyp
 	call	ReadString
 
+	mov		[bufsize], eax
+
+	pop		eax
 	pop		ecx
 	pop		edx
 
@@ -40,8 +47,9 @@ ENDM
 .data
 
 user_input		DWORD	ARRAYSIZE DUP(?)
-sum				DWORD	?
+sum_input		DWORD	?
 input_buffer	BYTE	12 DUP(?)
+input_size		BYTE	?
 program_title	BYTE	\
 "PROGRAMMING ASSIGNMENT 6: Designing low-level I/O procedures", 13, 10, 0
 program_auth	BYTE	"Written by: James Leflang", 13, 10, 0
@@ -81,9 +89,11 @@ main PROC
 	call	CrLf
 
 	push	SIZEOF input_buffer
+	push	OFFSET input_size
 	push	OFFSET input_buffer
+	push	OFFSET program_inperr
 	push	OFFSET program_prompt
-	push	ARRAYSIZE
+	push	SIZEOF user_input
 	push	OFFSET user_input
 	call	ReadVal
 
@@ -97,7 +107,7 @@ main PROC
 	call	WriteString
 
 	push	OFFSET user_input
-	push	sum
+	push	sum_input
 	call	Sum
 
 	call	CrLf
@@ -106,7 +116,7 @@ main PROC
 	call	WriteString
 
 	push	ARRAYSIZE
-	push	sum
+	push	sum_input
 	call	Avg
 
 	call	CrLf
@@ -161,10 +171,12 @@ ReadVal PROC
 ;	old ebp		ebp
 ;	ret @		ebp+4
 ;	input @		ebp+8
-;	input len	ebp+12
+;	input size	ebp+12
 ;	prompt		ebp+16
-;	buffer @	ebp+20
-;	buffer size	ebp+24
+;	error		ebp+20
+;	buffer @	ebp+24
+;	input_size	ebp+28
+;	buffer size	ebp+32
 ; Registers changed:
 ; Returns: None
 ;--------------------------------------
@@ -172,48 +184,87 @@ ReadVal PROC
 	push	ebp
 	mov		ebp, esp
 
-	mov		ecx, [ebp + 12]
-
-Input:
-
-	mov		edx, [ebp + 16]
-	call	WriteString
-
 	mov		esi, [ebp + 20]
 	mov		edi, [ebp + 8]
-	mov		edx, [ebp + 24]
 
-	getString esi, edx
+Input:
+	; Check if we are finished
+	mov		eax, [ebp + 12]
+	add		eax, [ebp + 8]
+	cmp		edi, eax
+	jg		Finish
 
+	; Get the user input
+	getString esi, [ebp + 32], [ebp + 28], [ebp + 16]
+
+	; Setup to process the input
 	xor		eax, eax
+	xor		ebx, ebx
+	mov		bl, 1
 
-	push	ecx
+	; Prepare the loop
+	std
 	mov		ecx, [ebp + 28]
-	dec		ecx
 
-	cld
+L1:			; LOOP: For each char in the string
+	; load the last value
 	lodsb
-	
 
+	; Skip the null
+	cmp		al, 0
+	je		L1
+
+	; Check for a negative sign
+	cmp		al, 45
+	jne		Cont
+
+	; Two's compliment
+	mov		eax, [edi]
+	xor		eax, 0
+	add		eax, 1
+
+	; Store the value and we are ready for the next input
+	mov		[edi], eax
+	add		edi, 4
+	jmp		Input
+
+Cont:		; Check to make sure the input is an integer value
 	cmp		al, 48
-	jl		OutRange
+	jl		Inval
 
 	cmp		al, 57
-	jg		OutRange
+	jg		Inval
 
+	; Store it
 	sub		al, 48
-	movzx	edx, al
-	mul		eax, 10
-	add		eax, edx
-	rep		movsb
+	mul		bl
+	stosb
 
-	pop		ecx
+	; 10 times the current multiplier
+	mov		ah, al
+	mov		al, bl
+	mov		bl, 10
+	mul		bl
+	mov		bl, al
+	mov		al, ah
+
+	; loop until the current input is transfered
+	loop	L1
+
+	; Go to the next number in the array
+	add		edi, 4
+	jmp		Input
+
+Inval:		; Invalid input
+	mov		edx, [ebp + 20]
+	call	WriteString
 
 	call	CrLf
-	loop	Input
+	jmp		Input
 
+Finish:
 	pop		ebp
-	ret		20
+	ret		28
 ReadVal ENDP
 
 ;--------------------------------------
